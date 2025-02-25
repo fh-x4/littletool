d4e8260bf2c282d9219aadc396a18f3ae5c00053
arbiter <xiaoxiaoarbiter@gmail.com>
2025-02-25 20:04:33 +0800

    feature: complete hbr damage caculator

--- server/handler/hbr/caculator.go
+++ server/handler/hbr/caculator.go
@@ -16,23 +16,38 @@ type damageCaculateReq struct {
 		DestructionRate float64 `json:"destruction_rate"`
 		MultiDefence    float64 `json:"multi_defence"`
 		MultiFragile    float64 `json:"multi_fragile"`
+		WeakPoint       []struct {
+			Types     string  `json:"types"`
+			SubTypes  int     `json:"sub_type"`
+			BoostRate float64 `json:"boost_rate"`
+		} `json:"weak_point"`
+		Resist []struct {
+			Types     string  `json:"types"`
+			SubTypes  int     `json:"sub_type"`
+			BoostRate float64 `json:"boost_rate"`
+		} `json:"resist"`
 	} `json:"cancer"`
 	Nabi struct {
 		Strength            int     `json:"strength"`
 		Agile               int     `json:"agile"`
 		FieldBoost          float64 `json:"field_boost"`
 		CriticalRate        int     `json:"critical_rate"`
-		CriticalDamageBoost int     `json:"critical_damage_boost"`
+		CriticalDamageBoost float64 `json:"critical_damage_boost"`
 		AttackBoost         float64 `json:"attack_boost"`
 		MindBoost           float64 `json:"mind_boost"`
 		HitNum              int     `json:"hit_num"`
 		HitRate             float64 `json:"hit_rate"`
 	} `json:"nabi"`
 	Skill struct {
-		MinPower    int     `json:"min_power"`
-		MaxPower    int     `json:"max_power"`
-		DpBoostRate float64 `json:"dp_boost_rate"`
-		HpBoostRate float64 `json:"hp_boost_rate"`
+		MinPower       int     `json:"min_power"`
+		MaxPower       int     `json:"max_power"`
+		PowerDiff      int     `json:"power_diff"`
+		WeightStrength int     `json:"weight_strength"`
+		WeightAgile    int     `json:"weight_agile"`
+		DpBoostRate    float64 `json:"dp_boost_rate"`
+		HpBoostRate    float64 `json:"hp_boost_rate"`
+		WeaponType     int     `json:"weapon_type"`
+		WeaponElem     int     `json:"weapon_elem"`
 	} `json:"skill"`
 }
 type damageCaculateRsp struct {
@@ -46,6 +61,55 @@ func (h *damageCaculateHandler) GetRespond() interface{} {
 	return h.rsp
 }
 func (h *damageCaculateHandler) Call(ctx context.Context) httpserver.IError {
+	enemy := cancer{
+		border:          h.req.Cancer.Border,
+		destructionRate: h.req.Cancer.DestructionRate,
+		defenceDown:     h.req.Cancer.MultiDefence,
+		fragile:         h.req.Cancer.MultiFragile,
+	}
+	friendly := nabi{
+		exskill: skill{
+			minPower:       h.req.Skill.MinPower,
+			maxPower:       h.req.Skill.MaxPower,
+			powerDiff:      h.req.Skill.PowerDiff,
+			weightStrength: h.req.Skill.WeightStrength,
+			weightAgile:    h.req.Skill.WeightAgile,
+			dpBoostRate:    h.req.Skill.DpBoostRate,
+			hpBoostRate:    h.req.Skill.HpBoostRate,
+			weaponType:     h.req.Skill.WeaponType,
+			weaponElem:     h.req.Skill.WeaponElem,
+		},
+		strength:            h.req.Nabi.Strength,
+		agile:               h.req.Nabi.Agile,
+		fieldBoost:          h.req.Nabi.FieldBoost,
+		criticalRate:        h.req.Nabi.CriticalRate,
+		criticalDamageBoost: h.req.Nabi.CriticalDamageBoost,
+		attackBoost:         h.req.Nabi.AttackBoost,
+		mindBoost:           h.req.Nabi.MindBoost,
+		hitNum:              h.req.Nabi.HitNum,
+		hitRate:             h.req.Nabi.HitRate,
+	}
+	for _, v := range h.req.Cancer.WeakPoint {
+		enemy.weakPoint = append(enemy.weakPoint, boostType{
+			types:     v.Types,
+			subType:   v.SubTypes,
+			boostRate: v.BoostRate,
+		})
+	}
+	for _, v := range h.req.Cancer.Resist {
+		enemy.resist = append(enemy.resist, boostType{
+			types:     v.Types,
+			subType:   v.SubTypes,
+			boostRate: v.BoostRate,
+		})
+	}
+
+	friendly.CaculateFinalPower(enemy)
+	h.rsp.Damage = int(float64(friendly.exskill.finalPower) *
+		friendly.CaculateWeakpointBoost(enemy) *
+		friendly.CaculateFriendlyBoost() *
+		enemy.CaculateEnemyBoost())
+
 	return nil
 }
 
--- server/handler/hbr/role.go
+++ server/handler/hbr/role.go
@@ -1,10 +1,37 @@
 package hbr
 
+const typeWeapon = "weapon"
+const typeElem = "element"
+
+const (
+	cut = iota
+	stabs
+	blunt
+
+	light
+	fire
+	dark
+	ice
+	thunder
+)
+
 type cancer struct {
 	border          int
 	destructionRate float64
-	multiDefence    float64
-	multiFragile    float64
+	defenceDown     float64
+	fragile         float64
+	weakPoint       []boostType
+	resist          []boostType
+}
+
+type boostType struct {
+	types     string
+	subType   int
+	boostRate float64
+}
+
+func (c *cancer) CaculateEnemyBoost() float64 {
+	return c.destructionRate * c.defenceDown
 }
 
 type nabi struct {
@@ -15,7 +42,7 @@ type nabi struct {
 	fieldBoost float64
 	// critical
 	criticalRate        int // %
-	criticalDamageBoost int
+	criticalDamageBoost float64
 	// boost
 	attackBoost float64
 	mindBoost   float64
@@ -25,10 +52,85 @@ type nabi struct {
 }
 
 type skill struct {
-	minPower    int
-	maxPower    int
+	minPower       int
+	maxPower       int
+	powerDiff      int
+	weightStrength int
+	weightAgile    int
+
+	finalPower  int
 	dpBoostRate float64
 	hpBoostRate float64
+	weaponType  int
+	weaponElem  int
 }
 
-// func (n *nabi) GetFinalPower(c cancer) int {}
+func (n *nabi) CaculateFinalPower(c cancer) {
+	weightBorder := (n.exskill.weightStrength*n.strength + n.exskill.weightAgile*n.agile) /
+		(n.exskill.weightStrength + n.exskill.weightAgile)
+	cancerBorder := c.border
+	if n.criticalRate > 100 {
+		cancerBorder -= 50
+	}
+
+	if weightBorder > cancerBorder {
+		if weightBorder-cancerBorder >= n.exskill.powerDiff {
+			n.exskill.finalPower = n.exskill.maxPower
+		} else {
+			n.exskill.finalPower = (n.exskill.maxPower-n.exskill.minPower)*((weightBorder-cancerBorder)/n.exskill.powerDiff) +
+				n.exskill.minPower
+		}
+	} else {
+		if cancerBorder-weightBorder >= n.exskill.powerDiff/2 {
+			n.exskill.finalPower = 1
+		} else {
+			n.exskill.finalPower = n.exskill.minPower * (1 - (cancerBorder-weightBorder)/(n.exskill.powerDiff/2))
+		}
+	}
+}
+
+func (n *nabi) CaculateWeakpointBoost(c cancer) float64 {
+	var boost float64 = 1
+	for _, v := range c.weakPoint {
+		if v.types == typeWeapon {
+			if v.subType == n.exskill.weaponType {
+				boost *= v.boostRate
+			}
+		}
+		if v.types == typeElem {
+			if v.subType == n.exskill.weaponElem {
+				boost *= v.boostRate
+			}
+		}
+	}
+	for _, v := range c.resist {
+		if v.types == typeWeapon {
+			if v.subType == n.exskill.weaponType {
+				boost *= v.boostRate
+			}
+		}
+		if v.types == typeElem {
+			if v.subType == n.exskill.weaponElem {
+				boost *= v.boostRate
+			}
+		}
+	}
+	if boost > 1 {
+		boost *= n.mindBoost
+		boost *= c.fragile
+	}
+	return boost
+}
+
+func (n *nabi) CaculateFriendlyBoost() float64 {
+	base := n.attackBoost * float64(n.hitNum) * n.hitRate * n.criticalDamageBoost * n.fieldBoost
+	if n.exskill.hpBoostRate != 1 {
+		base *= n.exskill.hpBoostRate
+	} else if n.exskill.dpBoostRate != 1 {
+		base *= n.exskill.dpBoostRate
+	}
+	return base
+}
+
+// 94363533
+// 261396
